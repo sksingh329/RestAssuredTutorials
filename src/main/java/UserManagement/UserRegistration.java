7a60739150839b9bf4eb3858d88ebbb855bc29a5
Subodh Singh <sksingh329@gmail.com>
2023-02-11 15:04:34 +0530

    Declared variable as final

--- src/main/java/UserManagement/UserRegistration.java
+++ src/main/java/UserManagement/UserRegistration.java
@@ -6,11 +6,13 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class UserRegistration {
-    private Response response;
-    private String baseUri = "https://test-api.k6.io";
-    private String userRegister = "/user/register/";
+
+    private final String baseUri = "https://test-api.k6.io";
+    private final String userRegisterPath = "/user/register/";
 
     private String userRegistrationRequestBody;
+    private Response response;
+
     public UserRegistration(String userName, String password){
         RestAssured.baseURI = baseUri;
         userRegistrationRequestBody = "{\"username\": \""+userName+"\",\"password\": \""+password+"\"}";
@@ -20,7 +22,7 @@ public class UserRegistration {
         response = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .body(userRegistrationRequestBody)
-                .post(userRegister);
+                .post(userRegisterPath);
     }
 
     public int getStatusCode(){
